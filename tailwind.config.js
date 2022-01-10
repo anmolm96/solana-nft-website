c223dc09f3141404d94bc444db60e12602f98eb0
Anmol Maini <anmol.maini@gmail.com>
2022-01-09 21:10:04 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  plugins: [],
+};
